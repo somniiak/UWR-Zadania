2fb87e3d19d2401cdd5f8975a78fd6c069a1c309
Filemon3Q <ad6127@disroot.org>
2024-10-20 18:34:45 +0200

    Zadanie 2

---
+++ Lista03/Zadanie2.py
@@ -0,0 +1,23 @@
+import itertools
+
+# n - liczba siódemek
+# m - długość liczby
+n = 7
+m = 10
+
+
+# Pary (m - n) liczb do wypełnienia miejsc poza
+# siódemkami w następnych kombinacjach.
+nums = [list(i) for i in itertools.product('0123456789', repeat=(m - n))]
+
+# Kombinacje (m - n) liczb z n siódemkami
+# np. ['3, '5', '2', '7777777']
+results = []
+for num in nums:
+    num.append('7' * n)
+    num = [''.join(i) for i in itertools.permutations(num) if i[0] != '0']
+    for l in num:
+        results.append(l)
+
+results = sorted(set(results))
+print(results, len(results))
