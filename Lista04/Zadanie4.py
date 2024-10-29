5d634619c07b5e5ac0dec4f04851d787031ecb3e
Filemon3Q <ad6127@disroot.org>
2024-10-29 13:51:37 +0100

    Optymalizacja algorytmu Erastotenesa

--- Lista04/Zadanie4.py
+++ Lista04/Zadanie4.py
@@ -1,19 +1,15 @@
 def primeRange(n):
-    if n < 2:
-        return []
-    nums = list(range(2, n + 1))
-    i = 0
-    while i < len(nums):
-        if nums[i] != 0:
-            k = nums[i]
-        else:
-            i += 1
-        for num in nums[i + 1:]:
-            if num != 0 and num > k and num % k == 0:
-                nums[nums.index(num)] = 0
-        i += 1
+    nums = [0, 0] + [i for i in range(2, n + 1)]
+    for i in range(2, n + 1):
+        if nums[i]:
+            k = i + i
+            while k <= n:
+                nums[k] = 0
+                k += i
     return nums
 
 palindromy = lambda a, b: [i for i in primeRange(b) if i >= a and str(i) == str(i)[::-1]]
 
-print(palindromy(500, 1000))
+x = primeRange(10 ** 6)
+print(x)
+#print(palindromy(500, 1000))
