d053a82d4b274a35c3ae709a32c6deb83575210a
Filemon3Q <ad6127@disroot.org>
2024-11-07 22:04:37 +0100

    Usuwanie duplikatów z listy

--- Lista05/Zadanie4.py
+++ Lista05/Zadanie4.py
@@ -1,32 +1,8 @@
-######################################
-#  usun_duplikaty.py
-######################################
+def usun_duplikaty(n):
+    n = sorted(list(enumerate(n)), key=lambda n: n[1])
+    n = [n[0]] + [n[i] for i in range(1, len(n)) if n[i][1] != n[i - 1][1]]
+    return [i[1] for i in sorted(n)]
 
-# cel: usun_duplikaty([1,2,3,1,2,6,7,7,7,0]) -> [1,2,3,6,7,0] 
-
-def usun_duplikaty1(L): # wolna wersja
-    wynik = []
-    for e in L:
-        if e not in wynik:   # not (e in wynik)
-            wynik.append(e)
-           
-    return wynik
-    
-def usun_duplikaty2(L): # wersja ze zbiorami
-    wynik = []
-    elementy = set()
-    for e in L:
-        if e not in elementy: 
-            wynik.append(e)        
-            elementy.add(e)
-           
-    return wynik
-
-
-usun_duplikaty = usun_duplikaty2
-    
-print (usun_duplikaty([1,2,3,1,2,6,7,7,7,0]))
-
-N = 7
-nowy = usun_duplikaty( list(range(10 ** N)))
-print (len(nowy))
+L = [5,1,4,1,2,3,1,2,3,8,2,2,2,9,9,4]
+L = usun_duplikaty(L)
+print (L)
