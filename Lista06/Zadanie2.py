1e6a0d65bb64db9083775faa0385ed6fc85a1102
ArD <ad6127@disroot.org>
2024-11-19 13:46:09 +0100

    Update Zadanie2.py

--- Lista06/Zadanie2.py
+++ Lista06/Zadanie2.py
@@ -4,7 +4,7 @@ odwrotne_slowa = set(slowo[::-1] for slowo in slowa)
 
 # Elementy wspólne zbiorów
 res = list(slowa & odwrotne_slowa)
-# Usuwanie palindromów np. 'a', 'menem', ...
-# i poprawne sformatowanie wyniku np. 'latem-metal', ...
-res = [f'{i}-{i[::-1]}' for i in res if i != i[::-1]]
+res = set(tuple(sorted([i, i[::-1]])) for i in res)
+res = [f'{i[0]}-{i[1]}' for i in res]
 print(sorted(res))
+print(len(res))
