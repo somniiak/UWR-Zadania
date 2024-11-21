1dd2645877a42f34cbeb3223c4a652cfc4034214
Filemon3Q <ad6127@disroot.org>
2024-11-21 19:34:43 +0100

    Lista 7

--- Lista07/Zadanie4.py
+++ Lista07/Zadanie4.py
@@ -1,3 +1,23 @@
 from murek import *
+from random import randint
 
-murek('fffffffffrfffffcffffflfffffffffcrfffffl',10)  
+speed('fastest')
+for _ in range(4):
+    for _ in range(10):
+        p = str(randint(1, 6))
+        murek(p, 20)
+        murek('f', 20)
+    murek('r', 20)
+
+
+# Liczba pasków z kwadratów
+n = 19
+
+tracer(2)
+speed('fastest')
+boki = list(range(2, n + 1))
+
+sq_sides(n)
+for bok in boki:
+    murek('fc' * bok + 'r', 25)
+input()
--- Lista07/murek.py
+++ Lista07/murek.py
@@ -1,12 +1,27 @@
 from turtle import *
 from random import randint
 
+def gradient(bok):
+    gradient = []
+    a = list(getColor())
+    b = list(getColor())
+    for i in range(bok):
+        step = tuple(int(a[j] + (b[j] - a[j]) * (i / (bok - 1))) for j in range(3))
+        gradient.append(step)
+    return gradient
+
 def getColor():
     colormode(255)
     r = randint(0, 255)
     g = randint(0, 255)
     b = randint(0, 255)
-    fillcolor((r, g, b))
+    return (r, g, b)
+
+def chooseColor(a):
+    colormode(255)
+    colors = {'1': (255, 0, 255), '2': (213, 34, 83), '3': (0, 255, 255),
+              '4': (255, 128, 0), '5': (128, 128, 128), '6': (10, 200, 100)}
+    fillcolor(colors[a])
 
 def kwadrat(bok):
     begin_fill()
@@ -14,8 +29,18 @@ def kwadrat(bok):
         fd(bok)
         rt(90)
     end_fill()
-    
+
+n = 0
+i = 0
+colors = 0
+def sq_sides(s):
+    global n
+    n = s
+
 def murek(s,bok):
+    global i
+    global n
+    global colors
     for a in s:
         if a == 'f':
             kwadrat(bok)
@@ -29,17 +54,10 @@ def murek(s,bok):
         elif a == 'r':
             rt(90)
             fd(bok)
+        elif not a.isalpha():
+            chooseColor(a)
         elif a == 'c':
-            getColor()
-
-        
-color('black', 'yellow')
-
-ht()
-
-tracer(0,0) # szybkie rysowanie     
-murek('ffffffcfffrffffffcfffcflfffffffffrfcffffl',10)    
-murek(4 * 'fffffr', 14)    
-update() # uaktualnienie rysunku
-
-input()
+            if not colors:
+                colors = gradient(sum(list(range(2, n + 1))))
+            fillcolor(colors[i])
+            i += 1
