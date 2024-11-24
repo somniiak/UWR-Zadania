306a21f1857bc22164af33e451fc5d9ce4614449
Filemon3Q <ad6127@disroot.org>
2024-11-24 03:12:28 +0100

    Optymalizacja zadania 3

--- Lista07/Zadanie3.py
+++ Lista07/Zadanie3.py
@@ -1,24 +1,13 @@
 import re
 
 with open('popularne_slowa2023.txt', 'r') as f:
-    slowa = set(i.strip() for i in f.readlines())
+    slowa = set(map(str.strip, f))
 
 with open('lalka-tom-pierwszy.txt', 'r') as f:
-    lines = re.sub('[ ]{2,}', ' ', ' '.join([i.strip() for i in f.readlines()]))
-lines = [i.strip(',.?!-:;— ') for i in re.split('\w*[ĄąĆćĘęŁłŃńÓóŚśŻźŹż]\w*', lines) if re.sub('[\W ]+', '', i)]
+    lines = re.split('\w*[ĄąĆćĘęŁłŃńÓóŚśŻźŹż]\w*', ' '.join(map(str.strip, f)))
+lines = [re.sub('^\W+|[^a-zA-Z0-9!?-]+$', '', i) for i in lines if re.sub('[\W ]+', '', i)]
 
-res = []
-for line in lines:
-    count = 0
-    words = re.findall('\w+', line)
-    for word in words:
-        if word in slowa:
-            count += 1
-    if count == len(words):
-        while not line[0].isalpha():
-            line = line[1:]
-        res.append((len(re.sub(r'\W+', '', line)), line))
-res = sorted(set(res), reverse=True)
+res = sorted({(len(re.sub('\W+', '', line)), line) for line in lines if all(word in slowa for word in re.findall('\w+', line))}, reverse=True)
 
 for i in range(4):
     print(f'...{res[i][1]}...')
