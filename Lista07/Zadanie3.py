d5c9d7ee732c693cee48d9227c533645e1c1d1ab
Filemon3Q <ad6127@disroot.org>
2024-11-24 13:51:25 +0100

    Poprawa wyrażenia regularnego

--- Lista07/Zadanie3.py
+++ Lista07/Zadanie3.py
@@ -4,8 +4,8 @@ with open('popularne_slowa2023.txt', 'r') as f:
     slowa = set(map(str.strip, f))
 
 with open('lalka-tom-pierwszy.txt', 'r') as f:
-    lines = re.split('\w*[ĄąĆćĘęŁłŃńÓóŚśŻźŹż]\w*', ' '.join(map(str.strip, f)))
-lines = [re.sub('^\W+|[^a-zA-Z0-9!?-]+$', '', i) for i in lines if re.sub('[\W ]+', '', i)]
+    lines = re.split('\w*[ĄąĆćĘęŁłŃńÓóŚśŻźŹż]+\w*', ' '.join(map(str.strip, f)))
+lines = [re.sub('^\W+|[^a-zA-Z0-9!?]+$', '', i) for i in lines if re.sub('[\W ]+', '', i)]
 
 res = sorted({(len(re.sub('\W+', '', line)), line) for line in lines if all(word in slowa for word in re.findall('\w+', line))}, reverse=True)
 
