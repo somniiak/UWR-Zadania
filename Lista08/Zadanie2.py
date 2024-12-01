12853a2504303dae1cc61c95770a3cf85a9d1e3b
353548 <ad6127@disroot.org>
2024-12-01 20:03:03 +0100

    Użycie Counter

--- Lista08/Zadanie2.py
+++ Lista08/Zadanie2.py
@@ -1,10 +1,6 @@
-def ukladane(word, base):
-    letterCount = lambda word: {i: word.count(i) for i in set(word)}
-    word, base = letterCount(word), letterCount(base)
-    for k, v in word.items():
-        if not v <= base[k]:
-            return False
-    return True
+from collections import Counter
+
+ukladane = lambda x, y: Counter(x) <= Counter(y)
 
 x = ukladane('motyl', 'lokomotywa')
 print(x)
