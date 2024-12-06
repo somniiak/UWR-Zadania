a9092be3e2b4179a411aa5dd2e9b7433c44fcd8c
353548 <ad6127@disroot.org>
2024-12-06 11:17:13 +0100

    Lista 09

--- Lista09/Zadanie2.py
+++ Lista09/Zadanie2.py
@@ -1,74 +1,44 @@
-import turtle as t
+from collections import Counter, defaultdict
+from itertools import combinations
 
-def draw_square(k):
-    """Malowanie kwadratu."""
-    t.fillcolor('black')
-    t.begin_fill()
-    for _ in range(4):
-        t.forward(k)
-        t.right(90)
-    t.end_fill()
-    t.forward(k)
+def load_word_dict(file):
+    """Wczytanie listy słów z pliku tekstowego."""
+    with open(file, 'r', encoding='utf-8') as f:
+        return [word.strip().lower() for word in f.readlines()]
 
-def turtle_travel(kx, ky):
-    """Względne przemieszczenie, bez rysowania."""
-    t.penup()
-    x, y = t.pos()
-    x, y = x + kx, y + ky
-    t.goto(x, y)
-    t.pendown()
-
-def sierpinski(k, n):
-    """Rekurencyjny dywan Sierpińskiego."""
-    if n == 1:
-        # Pierwszy rząd
-        for _ in range(3):
-            draw_square(k)
-        turtle_travel(-3 * k, -k)
-
-        # Drugi rząd
-        draw_square(k)
-        turtle_travel(k, 0)
-        draw_square(k)
-        turtle_travel(-3 * k, -k)
-
-        # Trzeci rząd
-        for _ in range(3):
-            draw_square(k)
+def riddle(input_name):
+    """Generowanie par słów-krzyżówek."""
+    char_name = input_name.lower().replace(' ', '') # Usunięcie spacji i zamiana na małe litery.
+    char_count = Counter(char_name) # Liczba wystąpień liter w imieniu i nazwisku.
     
-    else:
-        # Pierwszy rząd (rekurencyjny)
-        for _ in range(2):
-            sierpinski(k / 3, n - 1)
-            turtle_travel(0, 2 * k / 3)
-        sierpinski(k / 3, n - 1)
-        turtle_travel(-3 * k, -k / 3)
+    setSorted = lambda x: ''.join(sorted(x)) # Klucze słownika jako posortowane znaki, niestety Counter jest niehaszowalny.
+    
+    valid_words = defaultdict(lambda: [])
+    valid_words.update({ # Słownik z komprehensji jest dodawany do defaultdict, aby zachować funkcję default.
+        setSorted(word): word
+        for word in word_dict
+        if Counter(word) < char_count
+    })
+    
+    trios = set()
+    for word1, word2 in combinations(valid_words.values(), 2): # Łączymy wszystkie dobre słowa w pary (wartości w słowniku).
+        word3 = valid_words[setSorted(char_count - Counter(word1 + word2))] # Wybieramy trzecie słowo z niewykorzystanych znaków.
+        if word3 and Counter(word1 + word2 + word3) == char_count: # Sprawdzamy że trzecie słowo istnieje i wszystkie wyrazy spełniają specyfikację.
+            if all(word not in input_name.lower().split() for word in (word1, word2, word3)): # Wejściowe dane nie mogą być w wyniku.
+                trios.add(tuple(sorted((word1, word2, word3)))) # Zapisujemy wyrazy jako posortowane krotki, żeby się nie powtarzały.
+    return trios
 
-        # Drugi rząd (rekurencyjny)
-        sierpinski(k / 3, n - 1)
-        turtle_travel(k, 2 * k / 3)
-        sierpinski(k / 3, n - 1)
-        turtle_travel(-3 * k, -k / 3)
+# Załadowanie słownika
+word_file = 'popularne_slowa2023.txt'
+word_dict = load_word_dict(word_file)
 
-        # Trzeci rząd (rekurencyjny)
-        for _ in range(3):
-            sierpinski(k / 3, n - 1)
-            turtle_travel(0, 2 * k / 3)
-        t.update()
+# Generowanie par krzyżówek
+name_original = 'Bolesław Leśmian'
+name_pairs = riddle(name_original)
 
-k = 200 # Długość boku kwadratu
-n = 5 # Głębokość rekurencji
-
-# Przyśpieszenie malowania
-t.speed('fastest')
-t.hideturtle()
-t.tracer(0, 1)
-
-# Przesunięcie siatki
-offset_x = (-3 * k) // 2
-offset_y = (3 * k) // 2
-turtle_travel(offset_x, offset_y)
-
-sierpinski(k, n)
-
-t.Screen().exitonclick()
+# Wypisywanie wyników
+counter = 1
+for name_pair in name_pairs:
+    print(*name_pair, end=',\t')
+    if not counter % 5: print()
+    counter += 1
--- Lista09/Zadanie3.py
+++ Lista09/Zadanie3.py
@@ -1,44 +1,20 @@
-from collections import Counter, defaultdict
-from itertools import combinations
+import re
 
 def load_word_dict(file):
     """Wczytanie listy słów z pliku tekstowego."""
     with open(file, 'r', encoding='utf-8') as f:
         return [word.strip().lower() for word in f.readlines()]
 
-def riddle(input_name):
-    """Generowanie par słów-krzyżówek."""
-    char_name = input_name.lower().replace(' ', '') # Usunięcie spacji i zamiana na małe litery.
-    char_count = Counter(char_name) # Liczba wystąpień liter w imieniu i nazwisku.
-    
-    setSorted = lambda x: ''.join(sorted(x)) # Klucze słownika jako posortowane znaki, niestety Counter jest niehaszowalny.
-    
-    valid_words = defaultdict(lambda: [])
-    valid_words.update({ # Słownik z komprehensji jest dodawany do defaultdict, aby zachować funkcję default.
-        setSorted(word): word
-        for word in word_dict
-        if Counter(word) < char_count
-    })
+def sy_la_by(slowo):
+    return re.findall(r'[b-df-hj-np-tv-xz]*[aeiouy]*', slowo.lower())[:-1]
 
-    trios = set()
-    for word1, word2 in combinations(valid_words.values(), 2): # Łączymy wszystkie dobre słowa w pary (wartości w słowniku).
-        word3 = valid_words[setSorted(char_count - Counter(word1 + word2))] # Wybieramy trzecie słowo z niewykorzystanych znaków.
-        if word3 and Counter(word1 + word2 + word3) == char_count: # Sprawdzamy że trzecie słowo istnieje i wszystkie wyrazy spełniają specyfikację.
-            if all(word not in input_name.lower().split() for word in (word1, word2, word3)): # Wejściowe dane nie mogą być w wyniku.
-                trios.add(tuple(sorted((word1, word2, word3)))) # Zapisujemy wyrazy jako posortowane krotki, żeby się nie powtarzały.
-    return trios
+def wyszukaj_rym(slowo, liczba_sylab):
+    return [word for word in word_dict if sy_la_by(word)[-liczba_sylab:] == sy_la_by(slowo)[-liczba_sylab:] and word != slowo]
 
-# Załadowanie słownika
+# Wczytaj plik ze słowami
 word_file = 'popularne_slowa2023.txt'
 word_dict = load_word_dict(word_file)
 
-# Generowanie par krzyżówek
-name_original = 'Bolesław Leśmian'
-name_pairs = riddle(name_original)
-
-# Wypisywanie wyników
-counter = 1
-for name_pair in name_pairs:
-    print(*name_pair, end=',\t')
-    if not counter % 5: print()
-    counter += 1
+# Wyszukiwanie rymów
+word = wyszukaj_rym('przenica', 2)
+print(word)
